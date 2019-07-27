a99794673090e6411646b6ba062d203ade6f674f
jenniferlai43 <jenniferlai43@gmail.com>
2019-07-27 19:06:07 -0400

    Added error handling for email route

--- controller.js
+++ controller.js
@@ -8,19 +8,38 @@ module.exports = (app, db) => {
 	app.post("/email", (req, res) => {
 		console.log(req.body);
 		Email.create({email: req.body.email}).then((email) => {
-			if(email) {
-				res.json({
-					success: true,
+			return res.json({
+				success: true,
+				message: {
+					type: "success",
+					header: "Email entered",
+					content: "Email has been entered into mailing list."
+				}
+			});
+		}).catch((err) => {
+			// Duplicate email
+			if (err["name"] == "SequelizeUniqueConstraintError")
+			{
+				return res.json({
+					success: false,
 					message: {
-						type: "success",
-						header: "Email entered",
-						content: "Email has been entered into mailing list."
+						type: "failure",
+						header: "Duplicate",
+						content: "Email already exists."
 					}
 				});
 			}
-		}).catch((err) => {
-			/*TODO: catch validation errors (unique, not null, valid email)*/
-			console.log(err);
+			// Invalid email or null
+			if (err["name"] == "SequelizeValidationError") {
+				return res.json({
+					success: false,
+					message: {
+						type: "failure",
+						header: "Invalid",
+						content: "Email is not valid."
+					}
+				});
+			}
 		});
 	});
 }
