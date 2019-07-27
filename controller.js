9feeee8fb622ee21de44a1d41d086364dd06de93
jenniferlai43 <jenniferlai43@gmail.com>
2019-07-27 18:44:19 -0400

    Added post email route

--- controller.js
+++ controller.js
@@ -1,5 +1,26 @@
-module.exports = (app) => {
+const { Email } = require("./models");
+
+module.exports = (app, db) => {
 	app.get("/", (req, res) => {
 		res.render("index.ejs")
+	});
+
+	app.post("/email", (req, res) => {
+		console.log(req.body);
+		Email.create({email: req.body.email}).then((email) => {
+			if(email) {
+				res.json({
+					success: true,
+					message: {
+						type: "success",
+						header: "Email entered",
+						content: "Email has been entered into mailing list."
+					}
+				});
+			}
+		}).catch((err) => {
+			/*TODO: catch validation errors (unique, not null, valid email)*/
+			console.log(err);
+		});
 	});
 }
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: false}));
 
 require('./controller')(app);
 
